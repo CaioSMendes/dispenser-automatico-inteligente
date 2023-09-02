8bd28c5c511de8f2a27554bf1872c8b62b1992f8
CaioSMendes <caioitallo.souzamendes@gmail.com>
2023-09-02 15:03:52 -0300

    up frontend

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -310,9 +310,4 @@ Devise.setup do |config|
   # When set to false, does not sign a user in automatically after their password is
   # changed. Defaults to true, so a user is signed in automatically after changing a password.
   # config.sign_in_after_change_password = true
-
-  config.scoped_views = true
-  config.parent_layout = 'devise_layout'
-  config.omniauth_callbacks = 'users/omniauth_callbacks'
-
 end
